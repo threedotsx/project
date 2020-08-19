87ae425ab24b11084407b0c881eb03f14c1bac8c
threedot <threedotsx@bk.ru>
2020-08-19 20:32:01 +0300

    Initial Commit

---
+++ FunBoxTrue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunBoxTrue
+//
+//  Created by threedotsx on 19.08.2020.
+//  Copyright © 2020 threedotsx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
